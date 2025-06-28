b8c7987cfd8a74d185b6541398296e29159de01f
Sietse Meijer <sietsemeijer@mini-van-sietse.home>
2025-06-28 18:41:57 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './/App.css';
+import logo from './rec-logo.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-container">
+      <h1>Hallo, welkom bij Real Estate Care</h1>
+      <p className="App-text">Voor al je inspectiewerkzaamheden.</p>
+      <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
 }
